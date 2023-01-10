24971de6c33e2234302da90e2f90bbc1e982784c
Vadim Grinshpun <vg23@cornell.edu>
2023-01-10 15:07:25 -0500

    initial commit

---
+++ keyboards/kinesis/keymaps/seabadger/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// place overrides here
+#define TAPPING_TERM 200 // default 200
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
+#endif
