84101d1f2358f0b322a86c08ff0d4f2d0fd1ffa1
Vadim Grinshpun <vg23@cornell.edu>
2023-03-10 14:06:07 -0500

    enable CAPS_WORD; add hyper-1 OSL shortcut for 1password

--- keyboards/kinesis/keymaps/seabadger/config.h
+++ keyboards/kinesis/keymaps/seabadger/config.h
@@ -8,3 +8,6 @@
 #undef IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #endif
+
+// caps word: https://docs.qmk.fm/#/feature_caps_word
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
--- keyboards/kinesis/keymaps/seabadger/rules.mk
+++ keyboards/kinesis/keymaps/seabadger/rules.mk
@@ -2,8 +2,11 @@ BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+# COMMAND_ENABLE = yes         # Commands for debug and configuration
 NKRO_ENABLE = yes
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+
+CAPS_WORD_ENABLE = yes
+COMMAND_ENABLE = no         # conflicts with caps words
